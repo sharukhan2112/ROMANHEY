35df25879491a17964413ba1167264053329bace
sharukhan2112 <52273354+sharukhan2112@users.noreply.github.com>
2019-07-16 11:58:05 +0530

    Create srk52.py

---
+++ srk52.py
@@ -0,0 +1,23 @@
+n1=int(input())
+if n1==1:
+    print("One")
+elif n1==2:
+    print("Two")
+elif n1==3:
+    print("Three")
+elif n1==4:
+    print("Four")
+elif n1==5:
+    print("Five")
+elif n1==6:
+    print("Six")
+elif n1==7:
+    print("seven")
+elif n1==8:
+    print("Eight")
+elif n1==9:
+    print("Nine")
+elif n1==10:
+    print("Ten")
+else:
+    print("no")
