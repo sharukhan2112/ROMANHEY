48026609c907023db766b10f589d559b9b8ade8a
sharukhan2112 <52273354+sharukhan2112@users.noreply.github.com>
2019-07-18 17:18:36 +0530

    Create srk66.py

---
+++ srk66.py
@@ -0,0 +1,10 @@
+ee1=int(input())
+fl=0
+if ee>2:
+    for i in range(3,int(ee1/2)):
+        if ee1%i==0:
+            fl=1
+            print("no")
+            break
+if fl==0 or ee1==2:
+    print("yes")
